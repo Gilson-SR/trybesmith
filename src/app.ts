45fec20bc017d374093511c6720a549d5516ed9d
Gilson-SR <gilsonsrodrigues87@gmail.com>
2023-02-06 16:22:26 -0300

    feat: endpoint para cadastro de produtos

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
-// initial commit
+import productsRouter from './routers/productsRouter';
+
 const app = express();
 
 app.use(express.json());
+app.use('/products', productsRouter);
 
 export default app;
